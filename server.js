ee280a4d3d79bf929584c377c2764d496819d0e6
diogojms <a29986@alunos.ipca.pt>
2023-12-12 19:53:08 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+// const swaggerAutogen = require('swagger-autogen')()
+// const swaggerUi = require('swagger-ui-express')
+// const swaggerFile = require('./swagger_output.json')
+
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
+mongoose.Promise = global.Promise;
+mongoose.connect(uri).then(() => { 
+    console.log("Successfully connected to MongoDB.");
+}).catch(err => {
+    console.error("Connection error", err);
+}) 
+
+// Middleware
+const app = express();
+app.use('/', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); //parse application/x-www-form-urlencoded
+
+// routes
+app.use('/stores', require('./Routes/storesRoutes'));
+
+let port=8086;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
